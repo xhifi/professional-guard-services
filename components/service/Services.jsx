c693660d63c442e4e67f4e74c019efb90f41a278
xhifi <shifa.newversion@gmail.com>
2022-03-02 16:57:50 +0500

    Service component moved to parent Services Component

---
+++ components/service/Services.jsx
@@ -0,0 +1,16 @@
+import Service from "components/service/Service";
+import React from "react";
+
+const Services = ({ data }) => {
+  return (
+    <div className="container-fluid py-5 px-3 px-lg-5">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        {data.map((v, i) => {
+          return <Service key={i} heading={v.heading} content={v.content} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from "components/layouts/Main";
 
 import Carousel from "components/carousel/Carousel";
 import CallToAction from "components/calltoaction/callToAction";
-import Service from "components/service/Service";
+import Services from "components/service/Services";
 
 import data from "data/pages.json";
 import { pullData } from "utils/pullData";
@@ -20,13 +20,7 @@ export default function Home() {
     <Layout>
       <Carousel />
       {homeData && <CallToAction heading={homeData.callToAction.title} content={homeData.callToAction.content} />}
-      <div className="container-fluid py-5 px-3 px-lg-5">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-          {servicesData.services.map((v, i) => {
-            return <Service key={i} heading={v.heading} content={v.content} />;
-          })}
-        </div>
-      </div>
+      <Services data={servicesData.services} />
     </Layout>
   );
 }
