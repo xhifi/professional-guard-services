37463c6021db093b4c042846dea8a92bfb27b01f
xhifi <shifa.newversion@gmail.com>
2022-03-02 16:52:22 +0500

    Major Overhaul See changes

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
 };
+const withSass = require("@zeit/next-sass");
+const withPurgeCss = require("next-purgecss");
 
+module.exportts = withSass(withPurgeCss());
 module.exports = nextConfig;
