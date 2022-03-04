873a0c11d97fc9104423191baf8da9ce4268a90d
xhifi <shifa.newversion@gmail.com>
2022-03-05 03:54:27 +0500

    .

--- components/feature/Feature.jsx
+++ components/feature/Feature.jsx
@@ -11,13 +11,13 @@ const Feature = ({ orientation, content, heading, image }) => {
             <p>{content}</p>
           </div>
           <div className="col px-0 d-flex align-items-center rounded-3 overflow-hidden shadow feature-img mb-3 mb-md-0 order-2 order-sm-1 order-md-2">
-            <Image src={image || placeholder} layout="intrinsic" width={509 * 2} height={339 * 2} quality={100} priority />
+            <Image src={image || placeholder} alt={heading} layout="intrinsic" width={509 * 2} height={339 * 2} quality={100} priority />
           </div>
         </div>
       ) : (
         <div className="row row-cols-1 row-cols-md-2 align-items-center">
           <div className="col p-0 d-flex align-items-center rounded-3 overflow-hidden shadow feature-img mb-3 mb-md-0">
-            <Image src={image || placeholder} layout="intrinsic" width={509 * 2} height={339 * 2} quality={100} priority />
+            <Image src={image || placeholder} alt={heading} layout="intrinsic" width={509 * 2} height={339 * 2} quality={100} priority />
           </div>
           <div className="col feature-content">
             <h3>{heading}</h3>
