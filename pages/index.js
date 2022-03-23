98d311ad92a8505d0ee64e5514be26420b20bb0b
xhifi <shifa.newversion@gmail.com>
2022-03-23 06:40:45 +0500

    trying something

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import { useEffect } from "react";
+import Head from "next/head";
+
 import Layout from "components/layouts/Main";
 
-import Carousel from "components/carousel/Carousel";
 import CallToAction from "components/calltoaction/callToAction";
 import Services from "components/service/Services";
 import ProgressBar from "components/progress/ProgressBar";
 import Accordion from "components/accordion/Accordion";
 import Features from "components/feature/Features";
-import Slide from "components/carousel/slide";
+import SlideX from "components/carousel/slide";
 
 import data from "data/pages.json";
 import { pullData } from "utils/pullData";
@@ -22,7 +23,15 @@ export default function Home() {
 
   return (
     <Layout>
-      <Slide />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com/" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Saira:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <SlideX />
       {homeData && <CallToAction heading={homeData.callToAction.title} content={homeData.callToAction.content} />}
       {servicesData.services && <Services data={servicesData.services} />}
       <Features data={homeData.features} />
